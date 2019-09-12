4d7b6bdd8dd3e2a039e9071e18d983866a3485ca
genesis55 <kneislercp@nih.gov>
2019-09-12 12:17:35 -0400

    Updating to latest

--- setup/initialize_site.sh
+++ setup/initialize_site.sh
@@ -26,21 +26,22 @@ chmod -R 775 /local/drupal/site/web/sites/default/files
 echo "chmod -R 664 /local/drupal/site/web/sites/default/s*"
 chmod -R 664 /local/drupal/site/web/sites/default/s*
 
-echo ""
-echo "*Add symbolic link to shared volume"
-echo "username = $username (i.e. the project name for docker)"
-echo "ln -s /mnt/s3fs/$username /local/drupal/site/web/sites/default/files/assets/s3fs"
+#echo ""
+#echo "*Add symbolic link to shared volume"
+#echo "username = $username (i.e. the project name for docker)"
+#echo "ln -s /mnt/s3fs/$username /local/drupal/site/web/sites/default/files/assets/s3fs"
 #ln -s /mnt/s3fs /local/drupal/site/web/sites/default/files/assets/s3fs
 #echo "ls -latr"
 #ls -latr
 
-echo "Import Config or Load Database if config doesn't work"
+#echo "Import Config"
 #echo "drush cim -y"
 #drush cim -y
+echo "Load Database"
 drush sql-cli < /local/drupal/site/database.sql
 
-echo "Adding server info"
+echo "Enable LDAP"
 
-./enable_ldap.sh
+.setup/enable_ldap.sh
 
 
