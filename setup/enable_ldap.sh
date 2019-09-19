71a82070a81475bb02fffadd89861d6c72fbabf7
genesis55 <chris.kneisler@nih.gov>
2019-09-19 13:52:17 -0400

    Added ldap

--- setup/enable_ldap.sh
+++ setup/enable_ldap.sh
@@ -7,5 +7,8 @@ echo ""
 echo "* Setting up ldap server and port, turning on ldap_authentication"
 drush cset ldap_servers.server.nci address $ldap_address -y
 drush cset ldap_servers.server.nci port $ldap_port -y
+
 echo "* Enable ldap_authentication"
 drush pm-enable ldap_authentication -y
+drush cset ldap_authentication.settings sids.nci nci -y
+
